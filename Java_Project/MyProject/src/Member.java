d697d6e7a6ffc704316101f58b43d687c492a652
seungmin1592 <tmdals1592@naver.com>
2021-05-24 15:58:01 +0900

    Create Member.java

---
+++ Java_Project/MyProject/src/Member.java
@@ -0,0 +1,22 @@
+class Calculator {
+	int add(int x, int y) {
+		return x + y;
+	}
+	int subtract(int x, int y) {
+		return x - y;
+	}
+	int multiply(int x, int y) {
+		return x * y;
+	}
+	double devide(int x, int y) {
+		return x / y;
+	}
+}
+public class Member {
+
+	public static void main(String[] args) {
+		System.out.println("김승민");
+		
+	}
+
+}
