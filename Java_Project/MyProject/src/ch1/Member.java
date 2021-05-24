b46689d491ef7e0b808c321e70776bb6db21953c
seungmin1592 <tmdals1592@naver.com>
2021-05-24 18:22:36 +0900

    Update Member.java

--- Java_Project/MyProject/src/ch1/Member.java
+++ Java_Project/MyProject/src/ch1/Member.java
@@ -2,6 +2,16 @@ package ch1;
 public class Member {
 	public static void main(String[] args) {
 		System.out.println("김승민");
+		
+		String name = "김승민";
+		int age = 25;
+		double height = 171; 
+		boolean hasBook = true;
+		
+		System.out.println("이름은" + name +"입니다");
+		System.out.println("나이는" + age +"입니다");
+		System.out.println("키는" + height +"입니다");
+		System.out.println("책 보유 여부 : " + hasBook);
 	}
 }
 
