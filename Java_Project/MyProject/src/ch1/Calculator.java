2d6d52f849da4c8d76d1a36629513898379f6c0f
seungmin1592 <tmdals1592@naver.com>
2021-05-24 16:04:48 +0900

    Create Calculator.java

---
+++ Java_Project/MyProject/src/ch1/Calculator.java
@@ -0,0 +1,15 @@
+package ch1;
+class Calculator {
+	int add(int x, int y) {
+		return x + y;
+	}
+	int subtract(int x, int y) {
+		return x - y;
+	}
+	int multiply(int x, int y) {
+		return x * y;
+	}
+	int devide(int x, int y) {
+		return x / y;
+	}
+}
