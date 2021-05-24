697ec3958239ef374cff4a5c2e3281171ca984c4
seungmin1592 <tmdals1592@naver.com>
2021-05-24 16:58:21 +0900

    Chapter01
    Chapter 1번 문제

--- Java_Project/MyProject/src/ch1/Calculator.java
+++ Java_Project/MyProject/src/ch1/Calculator.java
@@ -1,15 +1,28 @@
 package ch1;
 class Calculator {
-	int add(int x, int y) {
-		return x + y;
+	void add(int x, int y) {
+		System.out.println(x+y);
 	}
-	int subtract(int x, int y) {
-		return x - y;
+	void subtract(int x, int y) {
+		System.out.println(x-y);
 	}
-	int multiply(int x, int y) {
-		return x * y;
+	void multiply(int x, int y) {
+		System.out.println(x*y);
 	}
-	int devide(int x, int y) {
-		return x / y;
+	void devide(int x, int y) {
+		System.out.println(x/y);
+	}
+	public static void main(String[]args) {
+		// 인스턴스 생성 -> 클래스의 정의가 필요(필요한 변수, 필요한 메서드)
+		// 클래스이름 참조변수 = new 클래스이름
+		
+		Calculator cal = new Calculator();
+		cal.add(10,20);
+		cal.subtract(20, 10);
+		cal.devide(10, 3);
+		cal.multiply(2, 5);
+		
 	}
 }
+
+	
