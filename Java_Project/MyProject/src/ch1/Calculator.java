abd964e9d13647a698d7b45b63203104fb7e99e1
seungmin1592 <tmdals1592@naver.com>
2021-05-24 18:22:33 +0900

    Update Calculator.java

--- Java_Project/MyProject/src/ch1/Calculator.java
+++ Java_Project/MyProject/src/ch1/Calculator.java
@@ -1,4 +1,7 @@
 package ch1;
+
+import java.util.Scanner;
+
 class Calculator {
 	void add(int x, int y) {
 		System.out.println(x+y);
@@ -12,6 +15,13 @@ class Calculator {
 	void devide(int x, int y) {
 		System.out.println(x/y);
 	}
+	void circleR(double x) {
+		System.out.println(2 * 3.14 * x);
+	}
+	void circleW(double x) {
+		System.out.println(3.14 * x * x);
+	}
+	
 	public static void main(String[]args) {
 		// 인스턴스 생성 -> 클래스의 정의가 필요(필요한 변수, 필요한 메서드)
 		// 클래스이름 참조변수 = new 클래스이름
@@ -21,8 +31,11 @@ class Calculator {
 		cal.subtract(20, 10);
 		cal.devide(10, 3);
 		cal.multiply(2, 5);
-		
+		cal.circleR(4);
+		cal.circleW(4);
+
 	}
+	
 }
 
 	
