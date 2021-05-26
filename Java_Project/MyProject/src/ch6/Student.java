1346b701a8283afd227eab5e821eb55a279168f4
seungmin1592 <tmdals1592@naver.com>
2021-05-26 12:34:44 +0900

    Update Student.java

--- Java_Project/MyProject/src/ch6/Student.java
+++ Java_Project/MyProject/src/ch6/Student.java
@@ -45,15 +45,41 @@ public class Student {
 		this.mathNum = mathNum;
 	}
 	
-	void result () {
-		int total = korNum + engNum + mathNum;
-		float average = total / 3;
-		System.out.println(name + "의 총점은 " + total + "이며, 평균은" + average + "입니다." );
+	public int getSum() {
+		return getKorNum() + getEngNum() + getMathNum();
+	}
+	public int getAver() {
+		return (getKorNum() + getEngNum() + getMathNum()) / 3;
+	}
+	
+	void print () {
+		System.out.println("학생 이름 : " + getName());
+		System.out.println("국어 점수 : " + getKorNum());
+		System.out.println("영어 점수 : " + getEngNum());
+		System.out.println("수학 점수 : " + getMathNum());
+		System.out.println("총점은 " + getSum() + "이며, 평균은" + getAver() + "입니다." );
+		
 	}
 	
 	public static void main(String[] args) {
-		Student[] stu = new Student [10];
+		Student[] student = new Student [10];
 		
+		student[0] = new Student("1번 학생", 49,70,30);
+		student[1] = new Student("2번 학생", 59,45,89);
+		student[2] = new Student("3번 학생", 41,60,31);
+		student[3] = new Student("4번 학생", 42,18,32);
+		student[4] = new Student("5번 학생", 43,82,33);
+		student[5] = new Student("6번 학생", 44,35,34);
+		student[6] = new Student("7번 학생", 45,46,35);
+		student[7] = new Student("8번 학생", 46,98,36);
+		student[8] = new Student("9번 학생", 47,70,38);
+		student[9] = new Student("10번 학생", 100,90,80);
+		
+		for(int i=0; i<student.length; i++) {
+			student[i].print();
+			System.out.println("-----------------");
+		}
+
 	}
 
 }
