ffb3645dc728abd9b84bbed095b48baab94d92e3
seungmin1592 <tmdals1592@naver.com>
2021-05-27 16:28:17 +0900

    Ex
    Ex 01

---
+++ Java_Project/MyProject/src/Ex/Car.java
@@ -0,0 +1,35 @@
+package Ex;
+
+public class Car {
+	int gasolinegauge;
+	Car(int gas){
+		this.gasolinegauge = gas;
+	}
+}
+class HybridCar extends Car {
+	int electronicGauge;
+	HybridCar(int gas, int eg) {
+		super(gas);
+		electronicGauge = eg;
+	}
+	
+	HybridCar(int gas) {
+		super(gas);
+	}
+
+}
+class HybridWaterCar extends HybridCar {
+	int waterGauge;
+	
+	public HybridWaterCar(int gg, int eg) {
+		super(gg, eg);
+	}
+	public HybridWaterCar(int gg) {
+		super(gg);
+	}
+	public void showCurrentGauge() {
+		System.out.println("잔여 가솔린 : " + gasolinegauge);
+		System.out.println("잔여 가솔린 : " + electronicGauge);
+		System.out.println("잔여 가솔린 : " + waterGauge);
+	}
+}
