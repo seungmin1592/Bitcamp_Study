e99883a6c4998a866d1c947004eb6dec61230097
seungmin1592 <tmdals1592@naver.com>
2021-06-14 11:54:25 +0900

    SQL_JOIN
    SQL_JOIN

--- SQL/210611_join.sql
+++ SQL/210611_join.sql
@@ -1,6 +1,6 @@
 -- 2021.06.11
 
--- JOIN
+-- JOIN : 스키마의 합
 
 -- CROSS JOIN
 -- 스키마의 합 : 집합의 곱 연산과 같다
@@ -10,7 +10,13 @@ from emp, dept
 where emp.deptno = dept.deptno
 ;
 
--- 이름이 SCOTT인 살마의 부서명을 출력해봅시다. where e.ename = 'SCOTT'
+-- ANSI CROSS JOIN
+select *
+from emp cross join dept
+;
+
+
+-- 이름이 SCOTT인 사람의 부서명을 출력해봅시다. where e.ename = 'SCOTT'
 -- 출력해야 하는 컬럼을 가지는 테이블을 확인해보자
 -- 이름 : emp, 부서명 : dept
 select e.ename, d.dname
@@ -25,3 +31,22 @@ select e.empno, e.ename, e.mgr, m.ename
 from emp e, emp m
 where e.mgr = m.empno(+)
 ;
+
+
+-- NON EQUI JOIN
+-- 동등 비교가 아닌 크다 작다와 같은 비교로 조인의 조건을 작성
+
+select *
+from salgrade
+;
+
+select e.ename, e.sal, s.grade, s.losal, s.hisal
+from emp e, salgrade s
+-- where e.sal >= s.losal and e.sal <= s.hisal
+where e.sal between s.losal and s.hisal
+order by e.ename
+;
+
+
+
+
