3d99658298f9731e98f3823c3e2a6d5ebd8b8e54
seungmin1592 <tmdals1592@naver.com>
2021-06-16 12:48:17 +0900

    Update 210615_sql_Q7_ddl.sql

--- SQL/210615_sql_Q7_ddl.sql
+++ SQL/210615_sql_Q7_ddl.sql
@@ -20,11 +20,11 @@ create table phoneInfo_univ (
     idx number(6) 
         constraint phoneInfo_univ_idx_pk primary key,
     fr_u_major varchar2(20) 
-        default 'N',
-    fr_u_year number(1) default 1, 
+        default 'N' not null,
+    fr_u_year number(1) default 1 not null, 
         constraint phoneInfo_univ_ch check ( 0 < fr_u_year and fr_u_year < 5),
     fr_ref number(7) 
-        constraint phoneInfo_univ_fr_ref_fk references phoneInfo_basic(idx)
+        not null, constraint phoneInfo_univ_fr_ref_fk references phoneInfo_basic(idx)
 );
 
 
@@ -32,7 +32,7 @@ create table phoneInfo_com (
     idx number(6) 
         constraint phoneInfo_com_idx_pk primary key,
     fr_c_company varchar2(20) 
-        default 'N',
+        default 'N' not null,
     fr_ref number(6) 
-        constraint phoneInfo_com_fr_ref_fk references phoneInfo_basic(idx)
+        not null, constraint phoneInfo_com_fr_ref_fk references phoneInfo_basic(idx)
 );
