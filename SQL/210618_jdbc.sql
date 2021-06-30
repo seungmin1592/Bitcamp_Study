7c4e4dd9c93093c9588ad1760acc77cf1e3a7485
seungmin1592 <tmdals1592@naver.com>
2021-06-30 14:34:11 +0900

    Update 210618_jdbc.sql

--- SQL/210618_jdbc.sql
+++ SQL/210618_jdbc.sql
@@ -1,3 +1,14 @@
 -- 2021.06.18
 
--- 
+-- JDBC
+
+select *
+from dept01;
+
+insert into dept01 values (dept01_deptno_seq.nextval, 'dev', 'SEOUL');
+
+-- dept01 deptno에 입력할 일련번호 -> suquence
+create sequence dept01_deptno_seq
+start with 10
+increment by 10
+;
