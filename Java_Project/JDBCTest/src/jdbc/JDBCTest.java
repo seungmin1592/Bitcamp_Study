0f598cbd6b546a648aeba53137ff244d39c5e94f
seungmin1592 <tmdals1592@naver.com>
2021-07-20 12:52:51 +0900

    mySql
    mySql

--- Java_Project/JDBCTest/src/jdbc/JDBCTest.java
+++ Java_Project/JDBCTest/src/jdbc/JDBCTest.java
@@ -19,17 +19,26 @@ public class JDBCTest {
 		ResultSet rs = null;
 		// Statement -> preparedStatement : 성능 개선
 		PreparedStatement pstmt = null;
-		// 1. 드라이버 로드
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// 1. 드라이버 로드
+			// 오라클
+			// Class.forName("oracle.jdbc.driver.OracleDriver");
 			
+			// mySql
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
 			System.out.println("드라이버 로드 성공");
 			
 			// 2. 연결
+			// 오라클
 			// jdbc:oracle:thin:@localhost:1521:xe
-			String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:xe";
-			String user = "hr";
-			String pw = "tiger";
+			// String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:xe";
+			
+			// mySql
+			String jdbcUrl = "jdbc:mysql://localhost:3306/project?serverTimezone=UTC";
+			
+			String user = "bit";
+			String pw = "bit";
 			
 			conn = DriverManager.getConnection(jdbcUrl, user, pw);
 			System.out.println("데이터베이스 연결 성공");
@@ -41,7 +50,9 @@ public class JDBCTest {
 			int dno = 10;
 			String otype = "deptno";
 			
-			String sqlSelect = "select * from dept where deptno = " + dno + " order by " + otype;
+			// String sqlSelect = "select * from dept where deptno = " + dno + " order by " + otype;
+			
+			String sqlSelect = "select * from dept";
 			
 			rs = stmt.executeQuery(sqlSelect);
 			
---
+++ SQL/210720_mysql_1.sql
@@ -0,0 +1,9 @@
+INSERT INTO `project`.`test`
+(`memo`)
+VALUES
+('test memo2');
+
+UPDATE `project`.`test`
+SET
+`memo` = 'edit memo';
+
