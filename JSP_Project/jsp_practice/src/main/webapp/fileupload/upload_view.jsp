dc33957bfc8952484b4309c60b6f6cd47cf4fdda
seungmin1592 <70023536+seungmin1592@users.noreply.github.com>
2021-07-28 15:17:44 +0900

    FileUpload

---
+++ JSP_Project/jsp_practice/src/main/webapp/fileupload/upload_view.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<h1>파일 업로드</h1>
+	<h3>
+		<c:if test="${title ne null}">
+			Title : ${title}<br>
+		
+		</c:if>
+		<c:if test="${photo ne null }">
+			<img src="">
+			<c:url value="/upload/${photo }"></c:url>
+		</c:if>
+	</h3>
+
+</body>
+</html>
--- JSP_Project/reg-project/src/main/java/domain/Member.java
+++ JSP_Project/reg-project/src/main/java/domain/Member.java
@@ -9,13 +9,15 @@ public class Member {
 	private String memberid;
 	private String password;
 	private String membername;
+	private String memberphoto;
 	private Timestamp regdate;
 	
-	public Member(int idx, String memberid, String password, String username, Timestamp regdate) {
+	public Member(int idx, String memberid, String password, String username, String memberphoto, Timestamp regdate) {
 		this.idx = idx;
 		this.memberid = memberid;
 		this.password = password;
 		this.membername = username;
+		this.memberphoto = memberphoto;
 		this.regdate = regdate;
 	}
 	
@@ -61,20 +63,31 @@ public class Member {
 		this.regdate = regdate;
 	}
 	
+	
+	public String getMemberphoto() {
+		return memberphoto;
+	}
+
+	public void setMemberphoto(String memberphoto) {
+		this.memberphoto = memberphoto;
+	}
+
 	// java.sql.TimeStamp -> java.util.Date
 	public Date getDate() {
 		return new Date(getRegdate().getTime());
 	}
 
+	
+	
 	@Override
 	public String toString() {
-		return "Member [idx=" + idx + ", memberid=" + memberid + ", password=" + password + ", username=" + membername
-				+ ", regdate=" + regdate + "]";
+		return "Member [idx=" + idx + ", memberid=" + memberid + ", password=" + password + ", membername=" + membername
+				+ ", memberphoto=" + memberphoto + ", regdate=" + regdate + "]";
 	}
-	
-	// Member -> LoginInfo
+
+		// Member -> LoginInfo
 		public LoginInfo toLoginInfo() {
-			return new LoginInfo(this.idx, this.memberid, this.membername);
+			return new LoginInfo(this.idx, this.memberid, this.membername, this.memberphoto);
 		}
 	
 	
