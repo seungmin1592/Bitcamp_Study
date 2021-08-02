a9026d85701856e4f01232d6db3d09a0b1864b1e
seungmin1592 <70023536+seungmin1592@users.noreply.github.com>
2021-08-02 17:44:11 +0900

    Spring

---
+++ Spring_Project/Spring-Di/src/member/service/ChangePasswordService.java
@@ -0,0 +1,27 @@
+package member.service;
+
+import member.dao.Dao;
+import member.domain.Member;
+
+public class ChangePasswordService {
+	
+	Dao dao;
+	public ChangePasswordService(Dao dao) {
+		this.dao = dao;
+		System.out.println("ChangePasswordService 인스턴스 생성");
+	}
+	public void changePassword(String email, String oldPw, String newPw) throws Exception {
+		
+		Member member = dao.selectByEmail(email);
+		
+		if(member == null) {
+			throw new Exception("존재하지 않는 회원");
+		}
+		
+		member.changePassword(oldPw, newPw);
+		
+		
+		dao.update(member);
+	}
+
+}
---
+++ Spring_Project/Spring-Di/src/member/service/MemberRegService.java
@@ -0,0 +1,35 @@
+package member.service;
+
+import java.util.Date;
+
+import member.dao.Dao;
+import member.domain.Member;
+import member.domain.RegRequest;
+
+public class MemberRegService {
+	
+	//MemberRegService Dao dao = new MemberDao();
+	Dao dao;
+	
+	public MemberRegService(Dao dao) {
+		this.dao = dao;
+		System.out.println("MemberRegService 인스턴스 생성");
+	}
+	
+	public void regMember(RegRequest request) throws Exception {
+		
+		// 중복 이메일 체크
+		Member member = dao.selectByEmail(request.getEmail());
+		
+		if(member != null) {
+			throw new Exception();
+		}
+		
+		member = new Member(0, request.getEmail(), request.getPassword(), request.getName(), new Date());
+		
+		dao.insert(member);
+	}
+	
+	
+
+}
