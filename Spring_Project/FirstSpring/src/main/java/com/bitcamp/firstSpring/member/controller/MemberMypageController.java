58f53017eb833a121aab1850d8724922162f0975
seungmin1592 <70023536+seungmin1592@users.noreply.github.com>
2021-08-06 15:00:40 +0900

    Spring

---
+++ Spring_Project/FirstSpring/src/main/java/com/bitcamp/firstSpring/member/controller/MemberMypageController.java
@@ -0,0 +1,24 @@
+package com.bitcamp.firstSpring.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberMypageController {
+	
+	@RequestMapping("/member/mypage/{id}")
+	public String mypage(
+			@PathVariable("id") String uid,
+			Model model
+			) {
+		
+		System.out.println(uid);
+		
+		model.addAttribute("userid",uid);
+		
+		
+		return "member/mypage";
+	}
+}
